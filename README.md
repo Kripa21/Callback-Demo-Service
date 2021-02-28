64bd451ae716dcee8dfbff9e88b91124d09c0f73
kripashankar <kripariehu@gmail.com>
2021-02-28 16:37:04 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Callback-Demo-Service
+
+1. Create new spring boot  project from Spring Initializer.
+https://start.spring.io/
+
+
---
+++ src/main/java/com/ks/callbackservice/CallbackserviceApplication.java
@@ -0,0 +1,13 @@
+package com.ks.callbackservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallbackserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallbackserviceApplication.class, args);
+	}
+
+}
